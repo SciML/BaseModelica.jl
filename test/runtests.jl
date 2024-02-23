685670109f6e7c72dc444dce2163a869e44f7905
Christopher Rackauckas <accounts@chrisrackauckas.com>
2024-02-23 10:42:14 -0500

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,7 +9,7 @@ end
 
 @testset "Parsing and Conversion Tests" begin
     newton_path = joinpath(pathof(BaseModelica), "test", "testfiles", "NewtonCoolingBase.mo")
-    newton_cooling = parse_file("/home/jadonclugston/Documents/Work/dev/Modelica/BaseModelica.jl/test/testfiles/NewtonCoolingBase.mo")
+    newton_cooling = parse_file("testfiles/NewtonCoolingBase.mo")
     @test newton_cooling isa BaseModelica.BaseModelicaPackage
     newton_system = baseModelica_to_ModelingToolkit(newton_cooling.model)
     @test newton_system isa ODESystem
