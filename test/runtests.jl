a658b00162acb3da24873be173f6a66e411b10e5
jClugstor <jadonclugston@gmail.com>
2024-06-26 10:47:27 -0400

    test format

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,14 +8,12 @@ using Test, SafeTestsets
         BM = BaseModelica
         PC = BM.ParserCombinator
 
-
         arith_test = only(PC.parse_one("5 + 6*(45 + 9^2)^2", BM.arithmetic_expression))
         @test arith_test isa BM.BaseModelicaSum
         @test BM.eval_AST(arith_test) == 95261.0
 
-
         newton_path = joinpath(
-            dirname(dirname(pathof(BM))),"test", "testfiles", "NewtonCoolingBase.mo")
+            dirname(dirname(pathof(BM))), "test", "testfiles", "NewtonCoolingBase.mo")
         newton_cooling = BM.parse_file(newton_path)
         @test newton_cooling isa BM.BaseModelicaPackage
         newton_system = BM.baseModelica_to_ModelingToolkit(newton_cooling)
