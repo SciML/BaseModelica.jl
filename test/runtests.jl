b85b7c2add479ec20a03b4fa7bf248e6994f981a
jClugstor <jadonclugston@gmail.com>
2024-06-28 15:04:02 -0400

    test groups

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,26 +1,32 @@
 using Test, SafeTestsets
 
-@testset "Quality Assurance" begin
-    @safetestset "Quality Assurance" include("qa.jl")
+const GROUP = get(ENV,"GROUP", "All")
+
+if GROUP == "All" || GROUP == "Quality"
+    @testset "Quality Assurance" begin
+        @safetestset "Quality Assurance" include("qa.jl")
+    end
 end
 
-@testset "BaseModelica" begin
-    @safetestset "Parsing and Conversion Tests" begin
-        using BaseModelica
-        using ModelingToolkit
-        BM = BaseModelica
-        PC = BM.ParserCombinator
+if GROUP == "All" || GROUP == "Core"
+    @testset "BaseModelica" begin
+        @safetestset "Parsing and Conversion Tests" begin
+            using BaseModelica
+            using ModelingToolkit
+            BM = BaseModelica
+            PC = BM.ParserCombinator
 
-        arith_test = only(PC.parse_one("5 + 6*(45 + 9^2)^2", BM.arithmetic_expression))
-        @test arith_test isa BM.BaseModelicaSum
-        @test BM.eval_AST(arith_test) == 95261.0
+            arith_test = only(PC.parse_one("5 + 6*(45 + 9^2)^2", BM.arithmetic_expression))
+            @test arith_test isa BM.BaseModelicaSum
+            @test BM.eval_AST(arith_test) == 95261.0
 
-        newton_path = joinpath(
-            dirname(dirname(pathof(BM))), "test", "testfiles", "NewtonCoolingBase.mo")
-        newton_cooling = BM.parse_file(newton_path)
-        @test newton_cooling isa BM.BaseModelicaPackage
-        newton_system = BM.baseModelica_to_ModelingToolkit(newton_cooling)
-        @test newton_system isa ODESystem
-        @test parse_basemodelica("testfiles/NewtonCoolingBase.mo") isa ODESystem
+            newton_path = joinpath(
+                dirname(dirname(pathof(BM))), "test", "testfiles", "NewtonCoolingBase.mo")
+            newton_cooling = BM.parse_file(newton_path)
+            @test newton_cooling isa BM.BaseModelicaPackage
+            newton_system = BM.baseModelica_to_ModelingToolkit(newton_cooling)
+            @test newton_system isa ODESystem
+            @test parse_basemodelica("testfiles/NewtonCoolingBase.mo") isa ODESystem
+        end
     end
 end
