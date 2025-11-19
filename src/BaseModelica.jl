98baf7ff99fd058489a504958b09476ad4f5ce47
jClugstor <jadonclugston@gmail.com>
2025-11-18 21:19:11 -0500

    add an option for which parser to use

--- src/BaseModelica.jl
+++ src/BaseModelica.jl
@@ -12,18 +12,34 @@ include("antlr_parser.jl")
 include("evaluator.jl")
 
 """
-    parse_basemodelica(filename::String)::ODESystem
+    parse_basemodelica(filename::String; parser::Symbol=:julia)::ODESystem
 
 Parses a BaseModelica .mo file into a ModelingToolkit ODESystem.
+
+## Arguments
+- `filename::String`: Path to the .mo file to parse
+- `parser::Symbol=:julia`: Parser to use. Options:
+  - `:julia` - ParserCombinator parser (default)
+  - `:antlr` - ANTLR parser
 
 ## Example
 
 ```julia
+# Use ANTLR parser (default)
 parse_basemodelica("testfiles/NewtonCoolingBase.mo")
+parse_basemodelica("testfiles/NewtonCoolingBase.mo", parser=:antlr)
+# Use ParserCombinator parser
+parse_basemodelica("testfiles/NewtonCoolingBase.mo", parser = :julia)
 ```
 """
-function parse_basemodelica(filename::String)
-    package = parse_file(filename)
+function parse_basemodelica(filename::String; parser::Symbol=:antlr)
+    package = if parser == :antlr
+        parse_file_with_antlr(filename)
+    elseif parser == :julia
+        parse_file(filename)
+    else
+        error("Unknown parser: $parser. Use :julia or :antlr")
+    end
     baseModelica_to_ModelingToolkit(package)
 end
 
